022150de38cf3d724eaedc0a77bf0c931d954bb6
Zukuo <eesa@xoio.us>
2021-11-22 00:11:48 +0000

    added boss key

--- game.py
+++ game.py
@@ -1,41 +1,41 @@
 from tkinter import *
 from random import randint
 
-def moveShip(event):
-    global ship, shipImage, shipImages
+def movePlayer(event):
+    global player, playerModels
 
     # movement
     if event.keysym == "Left" or event.keysym == "a":
-        canvas.move(ship, -15, 0)
+        canvas.move(player, -15, 0)
     elif event.keysym == "Right" or event.keysym == "d":
-        canvas.move(ship, 15, 0)
+        canvas.move(player, 15, 0)
     elif event.keysym == "Up" or event.keysym == "w":
-        canvas.move(ship, 0, -15)
+        canvas.move(player, 0, -15)
     elif event.keysym == "Down" or event.keysym == "s":
-        canvas.move(ship, 0, 15)
+        canvas.move(player, 0, 15)
 
     # ship randomizer
-    elif event.keysym == "f":
-        s = canvas.coords(ship)
-        l = len(shipImages)-1
-        canvas.delete(ship)
-        shipImage = shipImages[randint(0,l)]
-        ship = canvas.create_image(s[0], s[1], image=shipImage)
+    elif event.keysym == "r":
+        s = canvas.coords(player)
+        l = len(playerModels)-1
+        canvas.delete(player)
+        playerImageRandom = playerModels[randint(0,l)]
+        player = canvas.create_image(s[0], s[1], image=playerImageRandom)
 
-def shipShoot(self):
+def playerShoot(self):
     global shotAvailable
 
     # create box for getting the coords of the ship
-    box = canvas.create_rectangle(canvas.bbox(ship), outline="")
+    box = canvas.create_rectangle(canvas.bbox(player), outline="")
     c = canvas.coords(box)
     halfx = (c[0] + c[2]) / 2
 
     if shotAvailable == 1:
         canvas.create_line(halfx,c[1]-30,halfx,c[1],width=4,fill="yellow",tag="shot")
         shotAvailable = 0
-        shipShootUpdate("shot")
+        playerShootUpdate("shot")
 
-def shipShootUpdate(name):
+def playerShootUpdate(name):
     global shotAvailable
 
     canvas.move(name,0,-20)
@@ -48,9 +48,23 @@ def shipShootUpdate(name):
             canvas.delete(name)
             shotAvailable = 1
 
-    # update animation when bullet still on screen
+    # update animation when bullet still on screen 
+    # using another if to prevent speeding up bullet
     if shotAvailable == 0:
-        window.after(40, shipShootUpdate, name)
+        window.after(40, playerShootUpdate, name)
+
+def bossKey(event):
+    global bossKeyToggle, bossKeyLabel, window
+    bossKeyToggle ^= 1 # alternate toggle between 1 and 0
+
+    if bossKeyToggle == 1:
+        bossKeyLabel.image = bossKeyImage
+        bossKeyLabel.place(relx=0.5, rely=0.5, anchor="center")
+        window.title("Microsoft Excel - Employees.xlsx")
+
+    elif bossKeyToggle == 0:
+        bossKeyLabel.place_forget()
+        window.title("Space Invaders")
 
 def setWindowDimensions(w,h):
     window = Tk()
@@ -73,19 +87,27 @@ canvas = Canvas(window, bg="black", width=width, height=height)
 canvas.pack()
 
 # create the player
-shipImages = [PhotoImage(file="ships/ship1.png").subsample(5,5),
-              PhotoImage(file="ships/ship2.png").subsample(6,6),
-              PhotoImage(file="ships/ship3.png").subsample(6,6)]
-shipImage = shipImages[0]
-ship = canvas.create_image(x, x, image=shipImage)
+playerModels = [PhotoImage(file="ships/ship1.png").subsample(5),
+                PhotoImage(file="ships/ship2.png").subsample(7),
+                PhotoImage(file="ships/ship3.png").subsample(7)]
+playerImage = playerModels[0]
+player = canvas.create_image(x, height-100, image=playerImage)
 shotAvailable = 1 # set shotAvailable to 1 at beginning of game
 
-score = "Score: "
-canvas.create_text(20, 20, anchor=NW, font="terminus 20 bold", text=score, fill="#3a852e")
-health = "Health: "
-canvas.create_text(20, 70, anchor=NW, font="terminus 20 bold", text=health, fill="#cc272a")
+# setup boss key
+bossKeyToggle = 0
+bossKeyImage = PhotoImage(file="misc/bosskey.png")
+bossKeyLabel = Label(canvas, image=bossKeyImage, height=height, width=width)
 
-window.bind("<Key>", moveShip)
-window.bind("<space>", shipShoot)
+# set score and health
+scoreText = "Score: "
+canvas.create_text(20, 20, anchor=NW, font="terminus 20 bold", text=scoreText, fill="#3a852e")
+healthText = "Health: "
+canvas.create_text(20, 70, anchor=NW, font="terminus 20 bold", text=healthText, fill="#cc272a")
+
+# bind keys
+window.bind("<Key>", movePlayer)
+window.bind("<Control-b>", bossKey)
+window.bind("<space>", playerShoot)
 
 window.mainloop()
