4a11e31ad8d406193f1431e06b0022b50bafff8b
Zukuo <eesa@xoio.us>
2021-11-23 01:15:19 +0000

    added asteroids and cheat codes

--- game.py
+++ game.py
@@ -4,16 +4,6 @@ from random import randint
 def playerControls(event):
     global player, playerModels
 
-    # movement / old
-    # if event.keysym in leftBinds:
-    #     canvas.move(player, -15, 0)
-    # elif event.keysym in rightBinds:
-    #     canvas.move(player, 15, 0)
-    # elif event.keysym in upBinds:
-    #     canvas.move(player, 0, -15)
-    # elif event.keysym in downBinds:
-    #     canvas.move(player, 0, 15)
-
     # ship randomizer
     if event.keysym == "r":
         s = canvas.coords(player)
@@ -21,6 +11,10 @@ def playerControls(event):
         canvas.delete(player)
         playerImageRandom = playerModels[randint(0,l)]
         player = canvas.create_image(s[0], s[1], image=playerImageRandom)
+
+    # shoot when space is pressed
+    if event.keysym == "space":
+        playerShoot(event)
 
 def generateMoveStatus():
     # create a dictionary of binds
@@ -44,6 +38,8 @@ def setMoveBinds():
         window.bind("<KeyRelease-%s>" % char, moveKeyReleased)
 
 def moveKeyPressed(event):
+    global playerSpeed
+
     # set bind true if pressed
     if event.keysym in leftBinds:
         moveStatus[leftBinds] = True
@@ -55,10 +51,10 @@ def moveKeyPressed(event):
         moveStatus[downBinds] = True
 
     # if bind is set to true then move player
-    if moveStatus[leftBinds] == True: canvas.move(player, -15, 0)
-    if moveStatus[rightBinds] == True: canvas.move(player, 15, 0)
-    if moveStatus[upBinds] == True: canvas.move(player, 0, -15)
-    if moveStatus[downBinds] == True: canvas.move(player, 0, 15)
+    if moveStatus[leftBinds] == True: canvas.move(player, -playerSpeed, 0)
+    if moveStatus[rightBinds] == True: canvas.move(player, playerSpeed, 0)
+    if moveStatus[upBinds] == True: canvas.move(player, 0, -playerSpeed)
+    if moveStatus[downBinds] == True: canvas.move(player, 0, playerSpeed)
 
 def moveKeyReleased(event):
     # set bind false if released
@@ -71,23 +67,23 @@ def moveKeyReleased(event):
     if event.keysym in downBinds:
         moveStatus[downBinds] = False
 
-def playerShoot(self):
+def playerShoot(event):
     global shotAvailable
 
     # create box for getting the coords of the ship
     box = canvas.create_rectangle(canvas.bbox(player), outline="")
     c = canvas.coords(box)
-    halfx = (c[0] + c[2]) / 2
+    halfX = (c[0] + c[2]) / 2
 
     if shotAvailable == 1:
-        canvas.create_line(halfx,c[1]-30,halfx,c[1],width=4,fill="yellow",tag="shot")
+        canvas.create_line(halfX,c[1]-30,halfX,c[1],width=4,fill="yellow",tag="shot")
         shotAvailable = 0
         playerShootUpdate("shot")
 
 def playerShootUpdate(name):
     global shotAvailable
 
-    canvas.move(name,0,-20)
+    canvas.move(name,0,-playerShootSpeed)
     canvas.update()
         
     if shotAvailable == 0:
@@ -100,7 +96,43 @@ def playerShootUpdate(name):
     # update animation when bullet still on screen 
     # using another if to prevent speeding up bullet
     if shotAvailable == 0:
-        window.after(40, playerShootUpdate, name)
+        window.after(10, playerShootUpdate, name)
+
+def drawAsteroid(tag):
+    global width, height, asteroidImage
+    randomX = randint(100, width-100)
+    asteroid = canvas.create_image(randomX, -50, image=asteroidImage, tag=tag)
+    asteroidUpdate(tag)
+
+def asteroidUpdate(name):
+    global height
+    canvas.move(name,0,7)
+    canvas.update()
+    asteroidCoords = canvas.coords(name)
+    if asteroidCoords:
+        if asteroidCoords[1] > height:
+            canvas.delete(name)
+            drawAsteroid(name)
+        else:
+            window.after(30, asteroidUpdate, name)
+
+def fastShooting(event):
+    global isFastShooting, playerShootSpeed, defaultPlayerShootSpeed
+    isFastShooting ^= 1
+
+    if isFastShooting == 1:
+        playerShootSpeed = 30
+    elif isFastShooting == 0:
+        playerShootSpeed = defaultPlayerShootSpeed
+
+def fastSpeed(event):
+    global isFastSpeed, playerSpeed, defaultPlayerSpeed
+    isFastSpeed ^= 1
+
+    if isFastSpeed == 1:
+        playerSpeed = 40
+    elif isFastSpeed == 0:
+        playerSpeed = defaultPlayerSpeed
 
 def bossKey(event):
     global bossKeyToggle, bossKeyLabel, window
@@ -132,7 +164,7 @@ height = 900
 x = width / 2
 y = height / 2
 window = setWindowDimensions(width, height)
-canvas = Canvas(window, bg="black", width=width, height=height)
+canvas = Canvas(window, bg="#2b2b2b", width=width, height=height)
 canvas.pack()
 
 # create the player
@@ -141,7 +173,20 @@ playerModels = [PhotoImage(file="ships/ship1.png").subsample(5),
                 PhotoImage(file="ships/ship3.png").subsample(7)]
 playerImage = playerModels[0]
 player = canvas.create_image(x, height-100, image=playerImage)
+
 shotAvailable = 1 # set shotAvailable to 1 at beginning of game
+defaultPlayerSpeed = 20
+defaultPlayerShootSpeed = 10
+
+playerSpeed = defaultPlayerSpeed
+playerShootSpeed = defaultPlayerShootSpeed
+isFastSpeed = 0
+isFastShooting = 0
+
+# asteroids
+asteroidModels = [PhotoImage(file="asteroids/asteroid1.png").subsample(3)]
+asteroidImage = asteroidModels[0]
+drawAsteroid("asteroid")
 
 # setup boss key
 bossKeyToggle = 0
@@ -165,6 +210,7 @@ setMoveBinds()
 
 window.bind("<Key>", playerControls)
 window.bind("<Control-b>", bossKey)
-window.bind("<space>", playerShoot)
+window.bind("<Control-f>", fastShooting) # cheat code
+window.bind("<Control-s>", fastSpeed) # cheat code
 
 window.mainloop()
